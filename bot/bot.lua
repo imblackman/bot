80484d6d2b6020dcf1cbcc1bc80e8f3a4c64a8c3
Firstcode2 <whitewolforg5@gmail.com>
2017-02-05 12:04:54 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -88,31 +88,18 @@ function create_config( )
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v1
+    info_text = [[》Source v1
 An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
 
 》Special thanks to :
 》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
+》And Team Members
 
 》Our channel :
-》@BeyondTeam《
+》@DJteamm《
 
 》Our website :
-》http://BeyondTeam.ir
+》http://bilakh.ir
 ]],
   }
   serialize_to_file(config, './data/config.lua')
